17c741c234e9626e02c44624f50b219d447864f2
mbouchett <mark@markbouchett.com>
2017-07-04 02:00:32 -0400

    needs page added

---
+++ browse/browseNeed.php
@@ -0,0 +1,92 @@
+<?php
+// need/addNeed.php
+$name = $_COOKIE['name'];
+$ID = $_COOKIE['ID'];
+$message = $_REQUEST['message'];
+include "../db.php";
+$subcat = $_REQUEST['subcat'];
+
+// load past needs
+$db= new mysqli('localhost', $db_user, $db_pw, $db_db);
+if($subcat) {
+	$sql = "SELECT * FROM `needs`
+	     LEFT JOIN `subcat`
+	     ON `needs`.`subcat_ID` = `subcat`.`subcat_ID`
+	     WHERE `needs`.`subcat_ID`=".$subcat." 
+	     ORDER BY `needs`.`need_date` DESC
+	     LIMIT 50";
+}else{
+	$sql = "SELECT * FROM `needs`
+	     LEFT JOIN `subcat`
+	     ON `needs`.`subcat_ID` = `subcat`.`subcat_ID` 
+	     ORDER BY `needs`.`need_date` DESC
+	     LIMIT 50";
+}
+        
+$result = mysqli_query($db, $sql); 						// create the query object
+mysqli_close($db); 											//close the connection
+if($result){
+	$needsCount=mysqli_num_rows($result); 				//How many records meet select
+	//Store the Results To A Local Array
+	for($i=0; $i<$needsCount; $i++){         			//Iniate The Loop
+		$need[$i] = mysqli_fetch_assoc($result);   	//Fetch and save The Current Record
+	}                                           		//Close The Loop
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Browse <?= $need[0]['subcat_name'] ?></title>
+
+  <!-- 
+  		This is needed for the calendar
+  		Date: <input name="theDate" type="text" id="datepicker"> <--Put this in your HTML
+  -->
+  
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <link rel="stylesheet" href="../css/need_style.css" type="text/css" />
+  <link rel="SHORTCUT ICON" href="../images/icon.png">
+  
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker(); 
+  } );
+  </script>
+<!-- End Calendars -->
+
+<!-- Temporary Style Sheet -->
+<style>
+td{border: black thin solid;}
+</style>
+
+</head>
+<body>
+<a href="../index.php"><img height="150" src="../images/logo.png" alt="My Resource Connect" /></a><br></br>
+
+<div>
+    <?php if($message){ ?>
+    <div class="error"><span class="icon-warning red"><?= $message ?></span></div>
+    <?php } ?>
+</div>
+
+<hr>
+Needs<br>
+<table>
+	<tr><td>Needed</td><td>Title</td><td>Description</td><td>Needed By</td><td>Pledge</td></tr>
+	<?php for($i=0; $i<$needsCount; $i++){ ?>
+	<tr>
+		<td><?= substr($need[$i]['need_date'],0,10) ?></td>
+		<td><?= $need[$i]['need_title'] ?></td>
+		<td><textarea name="description" rows="3" cols="50"><?= $need[$i]['need_description'] ?></textarea></td>
+		<td><?= $need[$i]['need_by'] ?></td>
+		<td><input type="button" value="Pledge" /></td>
+	</tr>
+	<?php }?>
+</table>
+</body>
+</html>
