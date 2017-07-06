00a6b1f5cb15ff0fe7765c2a4096324d6babd660
mbouchett <mark@markbouchett.com>
2017-07-05 23:09:00 -0400

    needs added to main page

--- index.php
+++ index.php
@@ -35,6 +35,28 @@ if($result){
 	   $subCat[$i] = mysqli_fetch_assoc($result);
 	}
 }  
+
+/* **************Load the 10 most recent needs************** */
+$db= new mysqli('localhost', $db_user, $db_pw, $db_db);
+
+	$sql = "SELECT `needs`.`need_ID`, `needs`.`org_ID`, `needs`.`need_date`, 
+		   `needs`.`need_title`, `needs`.`need_description`, `orgs`.`org_name`	
+		  FROM `needs`
+	     LEFT JOIN `orgs` ON `needs`.`org_ID` = `orgs`.`org_ID` 
+	     ORDER BY `needs`.`need_date` DESC
+	     LIMIT 10";
+        
+$result = mysqli_query($db, $sql); 						// create the query object
+mysqli_close($db); 											//close the connection
+if($result){
+	$needsCount=mysqli_num_rows($result); 				//How many records meet select
+	//Store the Results To A Local Array
+	for($i=0; $i<$needsCount; $i++){         			//Iniate The Loop
+		$need[$i] = mysqli_fetch_assoc($result);   	//Fetch and save The Current Record
+	}                                           		//Close The Loop
+}
+/* **************end Load the 10 most recent needs************** */
+
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -48,7 +70,10 @@ if($result){
 Find out what's needed in your area and how YOU can help.
 <?php if($name){ ?> 
  <a href="accountType.php" title="<?= $name ?>" >My Account</a>
-<?php if($type == "org") { ?> <br> <a href="need/" >Post A Need</a> <?php } ?>
+<?php if($type == "org") { ?> 
+<br> <a href="need/" >Post A Need</a> </br>
+<span style="font-size: 10px;">Currently Logged In As: <?= stripslashes($name) ?><a href="account/log_out.php"> (not you?)</a>
+<?php } ?>
 <?php }else{ ?>
 <a href="login/sign_in.php" >Sign In</a>
 <?php } ?>
@@ -75,8 +100,22 @@ Find out what's needed in your area and how YOU can help.
 		<?php } ?>				
 </div><br>
 <hr width="900px">
+What is needed right now!<br>
+<table>
+	<tr><td>Needed</td><td>Organization</td><td>Title</td><td>Description</td><td>Pledge</td></tr>
+	<?php for($i=0; $i<$needsCount; $i++){ ?>
+	<tr>
+		<td><?= substr($need[$i]['need_date'],0,10) ?></td>
+		<td><?= $need[$i]['org_name'] ?></td>
+		<td><?= $need[$i]['need_title'] ?></td>
+		<td><textarea name="description" rows="3" cols="50"><?= $need[$i]['need_description'] ?></textarea></td>
+		<td><input type="button" value="Pledge" /></td>
+	</tr>
+	<?php }?>
+</table>
+<hr width="900px">
 <?php if($name){ ?>
-<span style="font-size: 10px;">Currently Logged In As: <?= stripslashes($name) ?><a href="account/log_out.php"> (not you?)</a>
+
 <?php }?>
 </span>
 </body>
