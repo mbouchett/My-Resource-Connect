05ff6cda9d2f76e35bf585cdc4312ae03208b956
mbouchett <mark@markbouchett.com>
2017-07-15 00:25:05 -0400

    small change to process question 07/14/2017mb

--- browse/processQuestion.php
+++ browse/processQuestion.php
@@ -1,5 +1,6 @@
 <?php
 // processQuestion.php
+include "../db.php";
 $donor_ID = $_POST['donor_ID'];
 $question = $_POST['question'];
 $need_ID = $_POST['need_ID'];
@@ -21,9 +22,7 @@ if($result){
 
 // load donor
 $db= new mysqli('localhost', $db_user, $db_pw, $db_db);
-$sql = "SELECT *	
-		  FROM `donors` 
-	     WHERE `needs`.`donor_ID`=".$donor_ID;
+$sql = "SELECT * FROM `donors` WHERE `donor_ID`=".$donor_ID;
      
 $result = mysqli_query($db, $sql); 						// create the query object
 mysqli_close($db); 											//close the connection
@@ -32,12 +31,12 @@ if($result){
 }
 
 
-echo $donor_ID."<br>";
-echo $question."<br>";
-echo $need_ID."<br>";
-echo $need_ID."<br>";
-echo $need['org_email']."<br>";
-echo $donor['donor_email']."<br>";
+//echo $donor_ID."<br>";
+$question = wordwrap($question,70);
+$need['org_email'] = "mb8023731035@gmail.com";
+//echo $donor['donor_email']."<br>";
 
+//Send Mail
+mail($need['org_email'], "A donor has a question", $message);
 die;
 ?>
