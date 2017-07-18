056f92295cefa0d0edb3934e7c7164f00d07625f
mbouchett <mark@markbouchett.com>
2017-07-18 08:50:39 -0400

    process pledge complete 07/18/2017mb

--- browse/processPledge.php
+++ browse/processPledge.php
@@ -29,13 +29,13 @@ mysqli_close($db); 											//close the connection
 if($result){
 	$donor = mysqli_fetch_assoc($result);   			//Fetch and save The Current Record
 }
-// *** Build The Email ***
 
+// *** Build The Email ***
 // Set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <'.$donor[donor_email].'>' . "\r\n";
-$subject = $donor[donor_name]." has made a pledge";
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8\r\n";
+$headers .= "From: ".$donor[donor_email]."\r\n";
+$subject = "MyResourceConnect: ".$donor[donor_name]." has made a pledge";
 
 $message = wordwrap($message,70);
 
@@ -44,16 +44,14 @@ $to = "mb8023731035@gmail.com";
 $msg = "This pledge is for your need titled: ";
 $msg .= $need[need_title]."<br><br>";
 if($message) {
-	$msg .= "<span style=\"font-weight: bold;\">Donor's question:</span> <br>";
+	$msg .= "<span style=\"font-weight: bold;\">Donor's Comment:</span> <br>";
 	$msg .= $message . "<hr>";
 }
 $msg .= "<br><br> <a href =\"www.myresourceconnect.org/acceptPledge.php?need=".$need_ID."&donor=".$donor_ID."\" >Click Here to ACCEPT this pledge!</a></br>";
-$msg .= "<br><br> <a href =\"www.myresourceconnect.org/rejectPledge.php?need=".$need_ID."&donor=".$donor_ID."\" >or Here to REJECT this pledge!</a></br>";
+$msg .= "<br><a href =\"www.myresourceconnect.org/rejectPledge.php?need=".$need_ID."&donor=".$donor_ID."\" >or Here to REJECT this pledge!</a></br>";
 // *** End Build Email ***
-
 //Send Mail
 mail($to, $subject, $msg, $headers);
-
 
 header('location: ../index.php?alert=1');
 die;
--- browse/processQuestion.php
+++ browse/processQuestion.php
@@ -34,7 +34,7 @@ if($result){
 // Set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <'.$donor[donor_email].'>' . "\r\n";
+$headers .= "From: ".$donor[donor_email]."\r\n";
 $subject = "A potential donor has a question";
 
 $question = wordwrap($question,70);
@@ -47,7 +47,6 @@ $message .= "<span style=\"font-weight: bold;\">Donor's question:</span> <br>";
 $message .= $question . "<hr>";
  
 // *** End Build Email ***
-
 //Send Mail
 mail($to, $subject, $message, $headers);
 
